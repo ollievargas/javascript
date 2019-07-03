56287c184946fc774bb48d80422eb385e5fde4dc
ollievargas <49657659+ollievargas@users.noreply.github.com>
2019-07-02 23:52:29 -0500

    Add files via upload

---
+++ class.js
@@ -0,0 +1,29 @@
+class Human {
+    constructor() {
+        this.gender = "male";
+    }
+    
+    printGender() {
+        console.log(this.gender);
+    }
+}
+
+
+
+  //using classes in javascript
+class Person extends Human {
+    constructor() {
+
+        super();
+        this.name = "Ollie";
+        this.gender = "female";
+    }
+    printMyName() {
+
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
